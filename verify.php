176e7ce26115804e9a02d83a54929a5567d30759
597416 <52811432+597416@users.noreply.github.com>
2019-07-12 14:58:14 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'uP7nSheZ1KDTsozIOf1imjt16+Rfd3VIte/wUBh/w1Ya+TwmcVAByCM7VFZjczTyaSNchBnc1waLz/xY1WA/gHqOYrBsHyj+x/qa9UdGWt8Dmk4iFur26ppHlTqNJELbxoBMR7ccmut0G/zYBZafmAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
